fad2a6bb5909973683161ba9337ed9c6661ab0cc
ToobaAftabb <113778113+ToobaAftabb@users.noreply.github.com>
2024-05-21 09:08:41 +0500

    Update program.java

--- program.java
+++ program.java
@@ -1,6 +1,6 @@
 public class program{
     public static void main(String[] args){
         System.out.println("Hello world");
-        System.out.println("Tooba Aftab);
+        System.out.println("Tooba Aftab");
     }
 }
