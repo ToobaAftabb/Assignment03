7054c6cd0ce5f5d1cefd1f61df6302c385c61402
ToobaAftabb <113778113+ToobaAftabb@users.noreply.github.com>
2024-05-21 09:00:32 +0500

    Update program.java

--- program.java
+++ program.java
@@ -1,6 +1,6 @@
 public class program{
     public static void main(String[] args){
         System.out.println("Hello world");
-        System.out.println("Assignment 4");
+        System.out.println("Aqsa bibi");
     }
 }
